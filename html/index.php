c598f5090ba380622f5ea31ea8902449b6fb2df4
Mohamed El Amine <cancerchange@gmail.com>
2015-03-22 13:05:22 +0100

    added native buttons - snaps to index.php.

--- html/index.php
+++ html/index.php
@@ -5,6 +5,13 @@
 	<meta charset="UTF-8">
 </head>
 <body>
-	
+	<div class="buttons">
+		<button>Right</button>
+		<button>Wrong</button>
+	</div>
+	<div class="snaps">
+		<p>prédiliction</p>
+		<p>تفضيل</p>
+	</div>
 </body>
 </html>
