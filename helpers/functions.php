fb9c507cd45a1d0067c77b26a4f45977f7bcaf37
Mohamed El Amine <cancerchange@gmail.com>
2015-03-23 08:35:32 +0100

    making the pull function more clean but still static

--- helpers/functions.php
+++ helpers/functions.php
@@ -1,16 +1,17 @@
 <?php
-
 	function checkAnswer(){
 		# checks answer
 	}
 	function increase(){
 		# if the answer was right then
 	}
-	function pull($lang){
+	function pull(){
 		# gets an element out of the current directory and puts it as an argument to render(footer)
 		$current =simplexml_load_file('../models/current.xml');
-		$lang=$current->xpath("/words/word/{$lang}");
-		return $lang['0'];
+		$french=$current->xpath("/words/word/french");
+		$arabic=$current->xpath("/words/word/arabic");
+		$pull=array('french'=>$french['0'],'arabic'=>$arabic['0']);
+		return $pull;
 	}
 	function checkAndMove(){
 
--- html/index.php
+++ html/index.php
@@ -1,6 +1,5 @@
 <?php 
 	require('../helpers/functions.php');
 	render("header");
-	
-	render("footer",array('french'=>pull('french'),'arabic'=>pull('arabic')));
+	render("footer",pull());
  ?>
