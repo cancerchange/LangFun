1825c3e1b981bd1354ce51ef9a7304b06f1f67bb
Mohamed El Amine <cancerchange@gmail.com>
2015-03-23 07:10:53 +0100

    adding other functions placeholders

--- helpers/functions.php
+++ helpers/functions.php
@@ -1,12 +1,18 @@
-<?php 
+<?php
+	function checkAnswer(){
+		# checks answer
+	}
+	function pull(){
+		# gets an element out of the current directory and puts it as an argument to render(footer)
+	}
 	function increase(){
-
+		# if the answer was right then 
 	}
 	function checkAndMove(){
 
 	}
 	function checkAndAdd(){
-		
+
 	}
 	function render($template,$data = array()){
 		$path="../views/".$template.".php";
